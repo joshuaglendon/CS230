b92d6ffa8dd3ab883feb9e4795efb95a6db329ef
joshuaglendon <jbg0019@mix.wvu.edu>
2021-09-08 13:39:29 -0400

    Lab 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BottomNavBarComponent} from './footer/bottom-nav-bar.component';
+import { TopNavBarComponent } from './header/top-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    BottomNavBarComponent
+
   ],
   imports: [
     BrowserModule
