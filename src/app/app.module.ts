1a78940ec637d1f3d20cc891a904d103c4fdedfb
joshuaglendon <jbg0019@mix.wvu.edu>
2021-09-09 12:53:21 -0400

    Added 2 components and footer, added alert above navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AlertComponent } from './alert/alert.component';
 
 import { AppComponent } from './app.component';
 import { BottomNavBarComponent} from './footer/bottom-nav-bar.component';
 import { TopNavBarComponent } from './header/top-nav-bar.component';
+import { PictureComponent } from './picture/picture.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
-    BottomNavBarComponent
+    BottomNavBarComponent,
+    AlertComponent,
+    PictureComponent
 
   ],
   imports: [
---
+++ src/app/picture/picture.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-picture',
+    templateUrl: './picture.component.html'
+})
+export class PictureComponent {
+    
+}
